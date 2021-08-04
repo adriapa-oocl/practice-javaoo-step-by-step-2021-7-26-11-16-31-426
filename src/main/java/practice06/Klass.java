06a4dc959af79288bea75c5d7bb9eab57682958b
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-04 21:28:33 +0800

    [ST003-06][ADRIAPA]feat: Introduced Klass/Person/Student/Teacher class for Practice 06 process

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,20 @@
 package practice06;
 
+import java.text.MessageFormat;
+
 public class Klass {
+
+    private final int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return MessageFormat.format("Class {0}", number);
+    }
 }
--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Student {
+import java.text.MessageFormat;
+
+public class Student extends Person{
+
+    private Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return concatIntroString();
+    }
+
+    public String concatIntroString(){
+        return MessageFormat.format("{0} I am a Student. I am at {1}.",super.introduce(), klass.getDisplayName());
+    }
 }
