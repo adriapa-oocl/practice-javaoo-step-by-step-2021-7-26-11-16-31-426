63f75a8eab8a46acfa728308193050b8bdc7159a
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-04 20:48:22 +0800

    [ST003-05][ADRIAPA]feat: Introduced Person/Student/Teacher and integrated intro outputs for each classes for Practice 05 process

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,32 @@
 package practice05;
 
-public class Teacher {
+import java.text.MessageFormat;
+
+public class Teacher extends Person{
+
+    private Integer klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age){
+        super(name, age);
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if (klass != null)
+            return MessageFormat.format(concatIntroString()+" I teach Class {0}.", klass);
+        else
+            return concatIntroString()+" I teach No Class.";
+    }
+
+    public String concatIntroString(){
+        return super.introduce()+" I am a Teacher.";
+    }
 }
